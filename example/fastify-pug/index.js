b224c80fbbaf838dea35c7c93bae7fe726a9ccf0
Adriano Raiano <adriano@raiano.ch>
2020-04-21 10:43:15 +0200

    add fastify-pug example

---
+++ example/fastify-pug/index.js
@@ -0,0 +1,38 @@
+const fastify = require('fastify')
+const fastifyPug = require('fastify-pug')
+const i18next = require('i18next')
+const i18nextMiddleware = require('i18next-http-middleware')
+// const i18nextMiddleware = require('../../cjs')
+const Backend = require('i18next-fs-backend')
+// const Backend = require('../../../i18next-fs-backend')
+
+const app = fastify()
+app.register(fastifyPug, {views: 'views'})
+const port = process.env.PORT || 8080
+
+i18next
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    // debug: true,
+    backend: {
+      // eslint-disable-next-line no-path-concat
+      loadPath: __dirname + '/locales/{{lng}}/{{ns}}.json',
+      // eslint-disable-next-line no-path-concat
+      addPath: __dirname + '/locales/{{lng}}/{{ns}}.missing.json'
+    },
+    fallbackLng: 'en',
+    preload: ['en', 'de'],
+    saveMissing: true
+  })
+
+app.register(i18nextMiddleware.plugin, { i18next })
+// app.addHook('preHandler', i18nextMiddleware.handle(i18next))
+
+app.get('/', (req, res) => {
+  res.render('index.pug')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
