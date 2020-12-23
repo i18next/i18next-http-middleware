2e6e1a6035daec47e2c77b3b71c53cc9a98baebb
Сергей Фомин <aunimof@gmail.com>
2020-12-23 19:03:09 +0200

    global declare request

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,19 @@
 import * as express from "express";
 import * as i18next from "i18next";
 
-declare module "i18next-http-middleware" {
-  type I18next = i18next.i18n;
+type I18next = i18next.i18n;
 
-  type I18NextRequest = express.Request & {
-    language: string;
-    languages: string[];
-    i18n: i18next.i18n;
-    t: i18next.TFunction;
-  };
+type I18NextRequest = {
+  language: string;
+  languages: string[];
+  i18n: i18next.i18n;
+  t: i18next.TFunction;
+};
+
+declare global {
+  namespace Express {
+    interface Request extends I18NextRequest {}
+  }
 }
+
+declare module "i18next-http-middleware" {}
